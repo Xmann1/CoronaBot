58c4a3f7573215694992fb3679e93d97504b3423
xmann1 <jotjernshaugen@gmail.com>
2020-03-03 15:55:39 +0100

    Added bare bones discord bot things

--- coronabot.py
+++ coronabot.py
@@ -1,1 +1,13 @@
-print("Hello, world")
+import discord
+
+with open("token.txt") as fr:
+    token = fr.read()
+
+
+class CoronaBot(discord.Client):
+    def __init__(self):
+        super().__init__()
+
+
+client = CoronaBot()
+client.run(token)
