cc32642a51ea0f2bd8595fabd5408e30404f3a0f
xmann1 <jotjernshaugen@gmail.com>
2020-03-04 13:16:09 +0100

    Basic bot setup

--- coronabot.py
+++ coronabot.py
@@ -1,14 +1,13 @@
 import discord
+import os
 
-with open("token.txt") as fr:
-    token = fr.read()
+token = os.getenv("BOT_TOKEN")
 
 
 class CoronaBot(discord.Client):
     def __init__(self):
         super().__init__()
 
-# Comment
 
 client = CoronaBot()
 client.run(token)
