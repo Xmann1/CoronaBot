f77a4691b86da823991766cea81a4594e4f770e7
xmann1 <jotjernshaugen@gmail.com>
2020-03-04 14:15:52 +0100

    Added bot functionality

--- coronabot.py
+++ coronabot.py
@@ -1,3 +1,4 @@
+import datetime
 import discord
 import os
 
@@ -12,6 +13,7 @@ class CoronaBot(discord.Client):
         self.corona_emoji = "<:corona:684132221077946401>"
         self.protected_message_contents = ["(This message is not infectious)"]
         self.max_infection_time = 10
+        self.log_file = "./log.txt"
 
         self.bind_events()
 
@@ -25,25 +27,25 @@ class CoronaBot(discord.Client):
             await self.event_on_message(ctx)
 
     async def event_on_ready(self):
-        print(f"Bot started as {self.user.name}")
+        self.log(f"Bot started as {self.user.name}")
 
     async def event_on_message(self, msg):
         self.log_message(msg)
 
         corona_role = discord.utils.get(msg.guild.roles, name="Corona infected")
         if corona_role is None:
-            print("No corona role detected in server, attempting to create one")
+            self.log("No corona role detected in server, attempting to create one")
             try:
                 corona_role = await msg.guild.create_role(name="Corona infected", color=discord.Color(0xff0000))
                 await msg.guild.get_member(self.user.id).add_roles(corona_role)
 
             except PermissionError:
-                print("Permission error, need Manage roles permission")
+                self.log("Permission error, need Manage roles permission")
                 if msg.guild.id not in self.permission_denied_warned_servers:
                     self.permission_denied_warned_servers.append(msg.guild.id)
                     await msg.channel.send("I do not have permission to manage roles")
                 else:
-                    print("Suppressing warning since server has already been warned before")
+                    self.log("Suppressing warning since server has already been warned before")
 
             else:
                 await msg.channel.send("Corona has been detected in this server")
@@ -52,7 +54,7 @@ class CoronaBot(discord.Client):
             author_has_corona = msg.author in corona_role.members
             if not author_has_corona:
                 latest_messages = []
-                print("  Earlier messages:")
+                self.log("  Earlier messages:")
                 async for earlier_message in msg.channel.history(limit=3):
                     self.log_message(earlier_message, prefix="  > ")
                     latest_messages.append(earlier_message)
@@ -60,35 +62,42 @@ class CoronaBot(discord.Client):
                 preceding_message = latest_messages[1]
                 preceding_author = preceding_message.author
 
-                print(f"    Last message:")
+                self.log(f"    Last message:")
                 self.log_message(preceding_message, prefix="  > ")
 
                 preceding_author_is_me = self.user.id == preceding_author.id
 
                 if preceding_author_is_me and not self.is_infectious(preceding_message.content):
-                    print("Last message is by me and is not infectious")
+                    self.log("Last message is by me and is not infectious")
 
                 else:
                     preceding_author_has_corona = preceding_author in corona_role.members
                     if preceding_author_has_corona:
-                        print("Preceding author has corona")
+                        self.log("Preceding author has corona")
 
                         seconds_passed_since_preceding_message = msg.created_at.timestamp() - preceding_message.created_at.timestamp()
 
-                        print(f"{seconds_passed_since_preceding_message} seconds have passed since preceding message")
+                        self.log(f"{seconds_passed_since_preceding_message} seconds have passed since preceding message")
 
                         if seconds_passed_since_preceding_message < self.max_infection_time:
-                            print("USER GOT INFECTED!")
+                            self.log("USER GOT INFECTED!")
 
                             await msg.author.add_roles(corona_role)
                             await msg.channel.send(f"{self.corona_emoji * 3} **Uh oh! It looks like you have been infected :flushed:** (This message is not infectious) {self.corona_emoji * 3}")
 
                         else:
-                            print("Infection has died in this message")
+                            self.log("Infection has died in this message")
+
+    def log(self, message, end="\n"):
+        start = datetime.datetime.now().strftime("<%d/%m/%Y %H:%M:%S> ")
+        print(message)
+        if self.log_file:
+            with open(self.log_file, "a") as fa:
+                fa.write(start + message + end)
 
     def is_infectious(self, message_content):
         for protected_message_content in self.protected_message_contents:
-            print(protected_message_content, message_content)
+            self.log(protected_message_content, message_content)
             if protected_message_content in message_content:
                 return False
 
@@ -98,11 +107,11 @@ class CoronaBot(discord.Client):
     def log_message(msg, prefix=""):
         if msg.guild:
             if msg.author.display_name != msg.author.name:
-                print(f"{prefix}[{msg.guild.name}/{msg.channel.name}] {msg.author.name}#{msg.author.discriminator} ('{msg.author.display_name}'): {msg.content}")
+                self.log(f"{prefix}[{msg.guild.name}/{msg.channel.name}] {msg.author.name}#{msg.author.discriminator} ('{msg.author.display_name}'): {msg.content}")
             else:
-                print(f"{prefix}[{msg.guild.name}/{msg.channel.name}] {msg.author.name}#{msg.author.discriminator}: {msg.content}")
+                self.log(f"{prefix}[{msg.guild.name}/{msg.channel.name}] {msg.author.name}#{msg.author.discriminator}: {msg.content}")
         else:
-            print(f"{prefix}[DM/{msg.channel.recipient}] {msg.author.name}#{msg.author.discriminator}: {msg.content}")
+            self.log(f"{prefix}[DM/{msg.channel.recipient}] {msg.author.name}#{msg.author.discriminator}: {msg.content}")
 
 
 client = CoronaBot()
